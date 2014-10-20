3f5f1b660624c92bb4020548f0a6287b03d0ce35
kmajones <kaelynnj@engr-134-05.eastern.ewu.edu>
2014-10-20 13:09:36 -0700

    rounded corners

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+  
+  // Your Scripts
+$('#one').corner('sharp bottom');
+$('#two').corner('30px');
+$('#three').corner('dog tr');
+});
